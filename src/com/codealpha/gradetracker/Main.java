01eeccbbbe36e35796ebbafdf193c6f25acaeb5a
AbuHurera05 <junejoabuhurera52@gmail.com>
2026-07-31 06:29:06 +0500

    add new file

---
+++ src/com/codealpha/gradetracker/Main.java
@@ -0,0 +1,9 @@
+package com.codealpha.gradetracker;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 Starting Student Grade Tracker...");
+        GradeTracker tracker = new GradeTracker();
+        tracker.run();
+    }
+}
